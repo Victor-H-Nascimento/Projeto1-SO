ca4e04f29760061d6eea0e2f8e4afe56fee537a2
Murilo <murilopapa123@gmail.com>
2018-10-10 10:19:04 -0300

    com gitignore

--- .exemplo_crypto.ko.cmd
+++
@@ -1,1 +0,0 @@
-cmd_/home/murilo/Documents/GitHub/Projeto1-SO/exemplo_crypto.ko := ld -r -m elf_x86_64 -z max-page-size=0x200000 -T ./scripts/module-common.lds --build-id  -o /home/murilo/Documents/GitHub/Projeto1-SO/exemplo_crypto.ko /home/murilo/Documents/GitHub/Projeto1-SO/exemplo_crypto.o /home/murilo/Documents/GitHub/Projeto1-SO/exemplo_crypto.mod.o ;  true
